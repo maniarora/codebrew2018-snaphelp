3108f2337165445eeb7eae0748a5e71e0b052a7d
Arpit Bajaj <arpitbajaj98@gmail.com>
2018-03-24 17:49:45 +1100

    Add files via upload

--- snaphelp/seeds.js
+++ snaphelp/seeds.js
@@ -11,10 +11,26 @@ var data = [
         comment: "It sucks",
         lat: "289372973",
         long: "283979213",
+    },
+    {
+        id: 2,
+        username: "Ben",
+        photo: "http://www.melbourne.vic.gov.au/SiteCollectionImages/summer-transport460x340.jpg",
+        tag: "HealthCare",
+        comment: "It sucks",
+        lat: "289",
+        long: "283973",
     }
 ]
 
 function seedDB(){
+    Incident.remove({}, function(err){
+        if(err){
+            console.log(err);
+        } else {
+            console.log("removed");
+        }
+    });
     data.forEach(function(seed){
         Incident.create(seed, function(err, incident){
             if(err){
